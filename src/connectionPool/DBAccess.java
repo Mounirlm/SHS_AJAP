56a4312ff745d58446e64878b74ae81126425f66
Kadiax <kadiditoure@gmail.com>
2019-01-10 12:31:55 +0100

    dbAccess

--- src/connectionPool/DBAccess.java
+++ src/connectionPool/DBAccess.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-// permet de lire le fichier db properties qui contient les configurations de la base de données
 
 public class DBAccess {
-	//singleton pattern de verifier qu'il y a une seule et unique instance de cette class DBAcces
+	//singleton
+
 	private static DBAccess dbAccess = new DBAccess();
 	private static  String DB_DRIVER_CLASS;
 	private static String DB_URL;
