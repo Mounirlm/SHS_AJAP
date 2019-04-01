c4a698293ea15aa32c066659a0227bdbcee3d9ab
Kadia Toure <kadiditoure@gmail.com>
2019-04-01 02:32:13 +0200

    insert view

---
+++ com.shs.client/com/shs/client/view/CreateView.java
@@ -0,0 +1,82 @@
+package com.shs.client.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+
+public class CreateView extends JPanel {
+	private JLabel title;
+	private ColorsDimApp cdApp = new ColorsDimApp();//TODO METTRE EN STATIC
+	private JTextField jtfRoomType;
+	private JTextField jtfFloor;
+	public CreateView() {
+		super();
+		this.setLayout(new BorderLayout());
+		//title
+		title = new JLabel("New Secured Room");
+		title.setOpaque(true);
+		title.setBackground(Color.decode("#afabab"));
+		title.setForeground(cdApp.getBgApp());
+		title.setHorizontalAlignment(JLabel.CENTER);
+		title.setFont(new Font("Arial", Font.BOLD, 30));
+		title.setBorder(new LineBorder(cdApp.getBgTitle(), 2));
+		
+		//pCenter
+		JPanel pCenter = new JPanel(new GridLayout(2,1));
+		
+		JPanel pForm = new JPanel(new GridLayout(7, 1));
+		jtfRoomType = new JTextField("Bedroom");
+		jtfFloor = new JTextField();
+		
+		pForm.add(new JPanel());
+		
+		JLabel lbType =new JLabel("TYPE");
+		lbType.setOpaque(true);
+		lbType.setBackground(cdApp.getBgThem());
+		lbType.setForeground(cdApp.getBgApp());
+		lbType.setHorizontalAlignment(JLabel.CENTER);
+		lbType.setFont(new Font("Arial", Font.BOLD, 25));
+		pForm.add(lbType);
+		jtfRoomType.setFont(new Font("Arial", Font.BOLD, 16));
+		pForm.add(jtfRoomType);
+
+		pForm.add(new JPanel());
+		
+		JLabel lbFloor =new JLabel("FLOOR");
+		lbFloor.setOpaque(true);
+		lbFloor.setBackground(cdApp.getBgThem());
+		lbFloor.setForeground(cdApp.getBgApp());
+		lbFloor.setHorizontalAlignment(JLabel.CENTER);
+		lbFloor.setFont(new Font("Arial", Font.BOLD, 25));
+		pForm.add(lbFloor);
+		jtfFloor.setFont(new Font("Arial", Font.BOLD,16 ));
+		pForm.add(jtfFloor);
+		
+		
+		//Panel Valider
+		JPanel pValidate = new JPanel();
+		JButton validateButton = new JButton("INSERT");
+		validateButton.setBackground(cdApp.getBgThem());
+		validateButton.setForeground(cdApp.getBgApp());
+		validateButton.setHorizontalAlignment(JLabel.CENTER);
+		validateButton.setFont(new Font("Arial", Font.BOLD, 20));
+		pValidate.add("inserer",validateButton);
+		pForm.add(pValidate);
+		
+		
+		pCenter.add(pForm);
+		
+		this.add(title, BorderLayout.NORTH);
+		this.add(pCenter, BorderLayout.CENTER);
+		
+	}
+}
--- com.shs.client/com/shs/client/view/SupRoomView.java
+++ com.shs.client/com/shs/client/view/SupRoomView.java
@@ -1,6 +1,7 @@
 package com.shs.client.view;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 
@@ -13,6 +14,8 @@ public class SupRoomView extends JPanel{
 	private ColorsDimApp cdApp;
 	private JButton jbCreate;
 	private JButton jbRUD;
+	private CreateView pCreate;
+	
 	public SupRoomView() {
 		super();
 		cdApp = new ColorsDimApp();
@@ -34,8 +37,15 @@ public class SupRoomView extends JPanel{
 		//pTopb
 		jbCreate = new JButton("CREATE");
 		jbCreate.setBackground(cdApp.getBgThem());
+		jbCreate.setForeground(cdApp.getBgApp());
+		jbCreate.setFont(new Font("Arial", Font.BOLD, 25));
+		jbCreate.setBorder(new LineBorder(cdApp.getBgTitle(),2));
+		
 		jbRUD = new JButton("READ - UPDATE - DELETE");
 		jbRUD.setBackground(cdApp.getBgThem());
+		jbRUD.setForeground(cdApp.getBgApp());
+		jbRUD.setFont(new Font("Arial", Font.BOLD, 25));
+		jbRUD.setBorder(new LineBorder(cdApp.getBgTitle(), 2));
 		
 		pTopB.add(jbCreate);
 		pTopB.add(jbRUD);
@@ -44,6 +54,12 @@ public class SupRoomView extends JPanel{
 		
 		//pCenter
 		pCenter.setBackground(cdApp.getBgApp());
+		pCenter.setLayout(new CardLayout());
+		
+		pCreate = new CreateView();
+		
+		pCenter.add("create", pCreate);
+		
 		
 		this.add(pTop, BorderLayout.NORTH);
 		this.add(pCenter, BorderLayout.CENTER);
