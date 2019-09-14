3b19efa514c7609cfb53c7c52f981004fb45f7df
Mounirlm <mlemharzi@yahoo.fr>
2019-09-14 19:47:17 +0200

    add method getRoomWithPostion()

--- com.shs.server/com/shs/server/model/RoomManager.java
+++ com.shs.server/com/shs/server/model/RoomManager.java
@@ -1,4 +1,4 @@
-package com.shs.server.model;
+ package com.shs.server.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import com.shs.commons.model.Building;
+import com.shs.commons.model.Floor;
 import com.shs.commons.model.Room;
 import com.shs.commons.model.Type_Room;
 import com.shs.commons.model.Wing_Room;
@@ -36,7 +38,7 @@ public class RoomManager {
         	roomList.add(new Room(RS.getInt("id"),RS.getInt("floor"), RS.getInt("room_number"), RS.getInt("m2"),
         			new Type_Room(rstype_room.getInt("id"), rstype_room.getString("name")),
         			new Wing_Room(rswing_room.getInt("id"), rswing_room.getString("name")),
-        			RS.getInt("nb_doors"), RS.getInt("nb_windows")));
+        			RS.getInt("nb_doors"),RS.getInt("nb_windows")));
         	}
         	
         }
@@ -306,4 +308,56 @@ public class RoomManager {
 		rs.next();
 		return rs.getInt(1);
 	}
+
+	public static Room getRoomWithPostion() throws SQLException {
+		Statement Stmt = conn.createStatement();
+		Statement Stmt2 = conn.createStatement();
+		Statement Stmt3 = conn.createStatement();
+		Statement Stmt4 = conn.createStatement();
+		Statement Stmt5 = conn.createStatement();
+		Room room = null;
+		ResultSet RS=null;
+		ResultSet rstype_room=null;
+		ResultSet rswing_room=null;
+		ResultSet rsfloor=null;
+		ResultSet rsbuilding=null;
+		try {
+        RS = Stmt.executeQuery("SELECT * FROM room");
+        
+        if(RS.next()) {
+        	rstype_room=Stmt2.executeQuery("SELECT * FROM type_room WHERE id="+RS.getInt("fk_type_room"));
+        	rswing_room=Stmt3.executeQuery("SELECT * FROM wing_room WHERE id="+RS.getInt("fk_wing_room"));
+        	rsfloor=Stmt4.executeQuery("SELECT * FROM floor_map WHERE id="+RS.getInt("fk_floor_map"));
+        	rsbuilding=Stmt5.executeQuery("SELECT * FROM building WHERE id="+RS.getInt("fk_building"));
+        	
+        	if ( rstype_room.next() && rswing_room.next() && rsfloor.next() && rsbuilding.next()) {
+		        room=new Room(RS.getInt("id"),RS.getInt("floor"), RS.getInt("room_number"), RS.getInt("m2"),
+		    			new Type_Room(rstype_room.getInt("id"), rstype_room.getString("name")),
+		    			new Wing_Room(rswing_room.getInt("id"), rswing_room.getString("name")),
+	        			RS.getInt("nb_doors"), RS.getInt("nb_windows"),RS.getInt("x"),RS.getInt("y"),RS.getInt("width"),RS.getInt("height"),
+	        			new Floor(rsfloor.getString("name"),rsfloor.getString("image_path") ,
+	        			new Building(rsbuilding.getString("name"),rsbuilding.getString("type")) ) );
+        	}
+        }
+		}finally {
+	        // Closing
+				
+			if(RS!=null)
+		        try{RS.close();}catch(Exception e){e.printStackTrace();} 
+			if(rstype_room!=null)
+	        	try{rstype_room.close();}catch(Exception e){e.printStackTrace();} 
+	        if(rswing_room!=null)
+	        	try{rswing_room.close();}catch(Exception e){e.printStackTrace();}  
+	        if(Stmt!=null)
+	        	try{Stmt.close();}catch(Exception e){e.printStackTrace();} 
+	        if(Stmt2!=null)
+	        	try{Stmt2.close();}catch(Exception e){e.printStackTrace();} 
+	        if(Stmt3!=null)
+	        	try{Stmt3.close();}catch(Exception e){e.printStackTrace();}     
+		    
+		}
+        return room;
+	}
+		
+	
 }
