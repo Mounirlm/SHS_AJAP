2c2ac31f78d0e3932900a7b0a5e6a7a377dc20b3
Mounirlm <mlemharzi@yahoo.fr>
2019-09-18 14:54:02 +0200

    Displaying a building list thanks to buildingController, BuildingListView, BuildingClientHandler

--- com.shs.client/com/shs/client/view/MapView.java
+++ com.shs.client/com/shs/client/view/MapView.java
@@ -2,29 +2,68 @@ package com.shs.client.view;
 
 import java.awt.BorderLayout;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import javax.swing.JPanel;
 
+import com.shs.client.controller.BuildingController;
 import com.shs.client.controller.SensorController;
 import com.shs.client.view.sensor.SensorListView;
+import com.shs.commons.model.Building;
+import com.shs.commons.model.Floor;
 import com.shs.commons.model.Sensor;
 
 public class MapView extends JPanel {
 private SensorListView sensorList;
 private SensorController sensorController;
 
+// apres 
+private BuildingListView buildingList;
+private BuildingController buildingController;
+
+MapTotalView pTotal;
+
 public MapView() {
 	super();
+//	try {
+//	    sensorController = new SensorController();
+//		List<Sensor> allSensors = sensorController.getAllSensors();
+//		sensorList = new SensorListView(allSensors);
+//	} catch (IOException e) {
+//		sensorList = new SensorListView(null);
+//		e.printStackTrace();
+//	}
+//	this.add(sensorList, BorderLayout.CENTER);
+//	
+	/// /////////////////ici cest moun
+	
+	this.setLayout(new BorderLayout());	
+	
+	
 	try {
-	    sensorController = new SensorController();
-		List<Sensor> allSensors = sensorController.getAllSensors();
-		sensorList = new SensorListView(allSensors);
+		buildingController = new BuildingController();
+		List<Building> buildings=buildingController.getBuildingList();
+		buildingList= new BuildingListView(buildings);
+		this.add(buildingList,BorderLayout.CENTER);
+		
+	} catch (UnknownHostException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	} catch (IOException e) {
-		sensorList = new SensorListView(null);
+		// TODO Auto-generated catch block
+		//buildingList = new BuildingListView(null);
 		e.printStackTrace();
 	}
-	this.add(sensorList, BorderLayout.CENTER);
+	
+	
+	
+	
+	
+	//pTotal = new MapTotalView();
 }
 
+ public MapTotalView getTotalView() {
+	return pTotal;
+}
 }
